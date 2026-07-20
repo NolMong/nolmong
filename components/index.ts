ebd7888e71b6f806cda7b685e2661ae86f831311
JuhyunLee <hanaheart030@gmail.com>
2026-07-20 17:28:36 +0900

    feat: 플랜 타입 추가

--- components/index.ts
+++ components/index.ts
@@ -6,6 +6,10 @@ export { default as Tag } from './common/Tag';
 export { default as ProfileAvatar } from './common/ProfileAvatar';
 // cards
 export { default as TravelTestCard } from './ cards/TravelTestCard';
+export { default as PlaceCard } from './ cards/PlaceCard';
+export { default as DayCard } from './ cards/DayCard';
+export { default as PlanEditorCard } from './ cards/PlanEditorCard';
+export { default as PlaceListContainer } from './container/PlaceListContainer';
 // template
 export { default as Header } from './templates/Header';
 // modal
