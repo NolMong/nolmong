6576cf687297053962557e01539a457d8b28407e
김혜진 <khj000217@naver.com>
2026-07-23 13:45:59 +0900

    feat: 데이터 TravelCard에 넣기

--- components/cards/TravelCard.tsx
+++ components/cards/TravelCard.tsx
@@ -2,34 +2,80 @@
 
 import { Tag } from '@/components';
 import { X } from 'lucide-react';
+import { PlanType } from '@/api/getPlans';
+import { getRelativeTime } from '@/utils/getRelativeTime';
+import { locations } from '@/data/locations';
 import Link from 'next/link';
+import dayjs from 'dayjs';
 
 const WAYPOINTS = (
   <div>
-    <span className='font-bold mr-3 text-sub '>Day1</span>
-    <span className='mr-2'>제주국제공항</span>
-    <span className='mr-2'>제주샴발리5성급호텔</span>
-    <span className='mr-2'>감귤농장</span>
-    <span className='mr-2'>한라산정상</span>
-    <span className='mr-2'>유우우우명한</span>
-    <span className='mr-10'>제주고기국수</span>
+    <span className='font-bold mr-4 text-sub '>Day1</span>
+    <span className='mr-3'>제주국제공항</span>
+    <span className='mr-3'>제주샴발리5성급호텔</span>
+    <span className='mr-3'>감귤농장</span>
+    <span className='mr-3'>한라산정상</span>
+    <span className='mr-3'>유우우우명한</span>
+    <span className='mr-14'>제주고기국수</span>
 
-    <span className='font-bold mr-3 text-sub'>Day2</span>
-    <span className='mr-2'>호텔</span>
-    <span className='mr-2'>고오오오급 돼지 국밥</span>
-    <span className='mr-2'>이쁜 카페</span>
-    <span className='mr-2'>끼깔난 점심</span>
-    <span className='mr-2'>고급진 카페</span>
-    <span className='mr-2'>훌륭한 갈치 조림집</span>
+    <span className='font-bold mr-4 text-sub'>Day2</span>
+    <span className='mr-3'>호텔</span>
+    <span className='mr-3'>고오오오급 돼지 국밥</span>
+    <span className='mr-3'>이쁜 카페</span>
+    <span className='mr-3'>끼깔난 점심</span>
+    <span className='mr-3'>고급진 카페</span>
+    <span className='mr-3'>훌륭한 갈치 조림집</span>
   </div>
 );
 
-export default function TravelCard() {
+const Locations = ({
+  startLocation,
+  endLocations,
+}: {
+  startLocation: string;
+  endLocations: string[];
+}) => {
+  const engName = (location: string) => {
+    const foundLocation = locations.find((loc) =>
+      loc.options.find((c) => c.city === location),
+    );
+    const city = foundLocation?.options.find((c) => c.city === location);
+    return city ? city.eng : '';
+  };
+
+  return (
+    <div className='flex gap-3 items-center font-jalnan-gothic text-main text-[18px]'>
+      <div>
+        <div>{startLocation}</div>
+        <div className='text-[10px] text-muted -mt-1 font-sans'>
+          {engName(startLocation)}
+        </div>
+      </div>
+      <div>‣</div>
+      {endLocations.map((location, index) => (
+        <div key={index} className='flex gap-3 items-center'>
+          <div>
+            <div>{location}</div>
+            <div className='text-[10px] text-muted -mt-1 font-sans'>
+              {engName(location)}
+            </div>
+          </div>
+          {index < endLocations.length - 1 && <div>‣</div>}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default function TravelCard({ data }: { data?: PlanType }) {
+  console.log(data);
   return (
     <div className='box w-90 rounded-[10px] border-2 border-border overflow-hidden shadow-[0px_4px_10px_0px_#b5b5b540] group hover:border-primary hover:scale-105 transition-all duration-300'>
       {/* 카드 헤더 */}
       <div className='w-full px-3.5 py-2 bg-primary-light flex justify-between'>
-        <div className='text-[12px] text-muted'>No. 2026071548375</div>
+        <div className='text-[12px] text-muted'>
+          No. {data ? dayjs(data.created_at).format('YYYYMMDDHHmmssms') : ''}
+        </div>
 
         <X
           color='var(--color-muted)'
@@ -44,66 +90,46 @@ export default function TravelCard() {
       <div className='w-full px-4 pt-3 pb-2 bg-[#FEFFFD]'>
         {/* 목적지와 누구 */}
         <div className='w-full flex justify-between'>
-          <div className='flex gap-3 items-center'>
-            <div>
-              <div className='font-jalnan-gothic text-main text-[18px]'>
-                서울
-              </div>
-              <div className='text-[10px] text-muted mt-[-2px]'>Seoul</div>
-            </div>
+          <Locations
+            startLocation={data?.start_location || ''}
+            endLocations={data?.end_locations || []}
+          ></Locations>
 
-            <div className='font-jalnan-gothic text-main text-[18px]'>‣</div>
-
-            <div>
-              <div className='font-jalnan-gothic text-main text-[18px]'>
-                부산
-              </div>
-              <div className='text-[10px] text-muted mt-[-2px]'>Busan</div>
-            </div>
-
-            <div className='font-jalnan-gothic text-main text-[18px]'>‣</div>
-
-            <div>
-              <div className='font-jalnan-gothic text-main text-[18px]'>
-                제주도
-              </div>
-              <div className='text-[10px] text-muted mt-[-2px]'>Jejudo</div>
-            </div>
-          </div>
           <div>
             <div className='w-6 h-6 rounded-full bg-primary'></div>
           </div>
         </div>
         {/* 가는 날짜 */}
         <div className='flex gap-1.5 text-sm text-main mt-2'>
-          <div>2026년 07월 16일</div>
+          <div>{dayjs(data?.start_day).format('YYYY년 MM월 DD일')}</div>
           <div>‣</div>
-          <div>07월 19일</div>
+          <div>{dayjs(data?.end_day).format('MM월 DD일')}</div>
         </div>
         {/* 여행 제목 */}
-        <div className='mt-4 text-muted'>제주도 힐링 투어</div>
+        <div className='mt-4 text-muted'>{data?.title || ''}</div>
       </div>
       {/* 들르는 장소 */}
       <div className='w-full px-3.5 py-2 bg-primary-light overflow-hidden'>
         <div className='flex w-max group-hover:animate-marquee'>
           <div className='text-[12px] text-muted whitespace-nowrap pr-8'>
-            {WAYPOINTS}
+            {data?.cards.length === 0 ? '아직 일정이 없습니다.' : WAYPOINTS}
+            {/* {WAYPOINTS} */}
           </div>
-          <div
+          {/* <div
             className='text-[12px] text-muted whitespace-nowrap pr-8'
             aria-hidden='true'
           >
-            {WAYPOINTS}
-          </div>
+            {data?.cards.length === 0 ? '아직 일정이 없습니다.' : WAYPOINTS}
+          </div> */}
         </div>
       </div>
 
       {/* 언제 바꿨는지 & 수정버튼 */}
       <div className='w-full px-3.5 py-1.5 bg-[#FEFFFD] flex items-center justify-between'>
         <div className='text-[12px] text-muted whitespace-nowrap'>
-          최근 편집 : 방금 전
+          최근 편집 : {data ? getRelativeTime(data.updated_at) : ''}
         </div>
-        <Link href='/plan/1' className='flex gap-1 items-center'>
+        <Link href={`/plan/${data?.uuid}`} className='flex gap-1 items-center'>
           <Tag color='primary'>수정</Tag>
         </Link>
       </div>
