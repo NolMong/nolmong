dc13612ea39444a662057da1763a5e4939457350
JuhyunLee <hanaheart030@gmail.com>
2026-07-23 16:36:07 +0900

    feat: DndContext에 고유 아이디 부여

--- app/plan/[id]/_components/WishlistTab.tsx
+++ app/plan/[id]/_components/WishlistTab.tsx
@@ -88,6 +88,7 @@ export default function WishlistTab() {
 
   return (
     <DndContext
+      id="plan-dnd-context"
       sensors={sensors}
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
