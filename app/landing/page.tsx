bb8792da7f523269fb58a158a3480188bbaab9bd
김혜진 <khj000217@naver.com>
2026-07-24 09:39:11 +0900

    deploy: useSearchPararms Suspense로 감쌈

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { Header, Tag } from '@/components';
 import { CalendarDays, Map, Sheet, type LucideIcon } from 'lucide-react';
 import { useLoginModalStore } from '@/store/useModalStore';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 const features: {
@@ -122,133 +122,141 @@ export default function LandingPage() {
   }, [searchParams, router]);
 
   return (
-    <div>
-      {/* <Header /> */}
-      {/* 랜딩 이미지 */}
-      <div className='relative'>
-        <Image
-          src='/images/landing_bg.webp'
-          alt='Landing Image'
-          width={1512}
-          height={856}
-          className='h-auto w-full'
-          loading='eager'
-        />
-        <Image
-          src='/images/capi1.webp'
-          alt='Capi Image'
-          width={222}
-          height={295}
-          loading='eager'
-          className='absolute bottom-[-10%] left-[10%] w-[14.68%] h-auto -translate-y-1/2'
-        />
-        <Image
-          src='/images/bara1.webp'
-          alt='Bara Image'
-          width={222}
-          height={295}
-          loading='eager'
-          className='absolute bottom-[-10%] left-[27%] w-[14.68%] h-auto -translate-y-1/2'
-        />
-      </div>
-
-      {/* 첫번째 카드 */}
-      <div className='box bg-primary-light w-full py-15 font-jalnan'>
-        <div className='mb-4 text-2xl text-center text-primary-focus'>
-          공동 여행 계획 서비스
-        </div>
-        <div className='text-center text-4xl text-primary leading-[1.4]'>
-          친구들과 함께 여행 계획을
-          <br />
-          세워보세요!
-        </div>
-        {/* 나중에 화면 캡쳐해서 쓸거임 */}
-      </div>
-      {/* 두번째 카드 */}
-      <div className='box bg-white w-full py-15 font-jalnan'>
-        <div className='text-center text-4xl text-sub'>이런걸 도와드려요</div>
-
-        <div className='w-fit flex gap-7.5 mx-auto mt-15'>
-          {features.map(({ icon: Icon, bg, iconColor, title, description }) => (
-            <div
-              key={title}
-              className={`box w-80 h-72 rounded-[20px] px-10 py-7.5 ${bg} flex flex-col items-center gap-5`}
-            >
-              <div className='w-14 h-14 bg-white rounded-full flex items-center justify-center'>
-                <Icon className={`w-8 h-8 ${iconColor}`} />
-              </div>
-              <div className='text-[20px] text-center text-sub'>{title}</div>
-              <div className='font-sans font-normal text-center text-sub'>
-                {description}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* 세번째 카드 */}
-      <div className='box bg-caramel-light w-full py-15 font-jalnan'>
-        <div className='mb-4 text-2xl text-center text-brown'>
-          공동 여행 계획 서비스
-        </div>
-        <div className='mb-4 text-center text-4xl text-caramel'>
-          카피 & 바라를 소개합니다
-        </div>
-        <div className='font-sans font-normal text-center text-brown'>
-          서로 다른 여행 스타일을 가진 두 친구, 당신은 누구와 더 닮았나요?
-        </div>
-        <div className='flex gap-4 justify-center mt-15'>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        {/* <Header /> */}
+        {/* 랜딩 이미지 */}
+        <div className='relative'>
           <Image
-            src='/images/capi2.webp'
+            src='/images/landing_bg.webp'
+            alt='Landing Image'
+            width={1512}
+            height={856}
+            className='h-auto w-full'
+            loading='eager'
+          />
+          <Image
+            src='/images/capi1.webp'
             alt='Capi Image'
             width={222}
             height={295}
             loading='eager'
+            className='absolute bottom-[-10%] left-[10%] w-[14.68%] h-auto -translate-y-1/2'
           />
           <Image
-            src='/images/bara2.webp'
+            src='/images/bara1.webp'
             alt='Bara Image'
             width={222}
             height={295}
             loading='eager'
+            className='absolute bottom-[-10%] left-[27%] w-[14.68%] h-auto -translate-y-1/2'
           />
         </div>
-        <div>
-          <div className='flex gap-15 mt-15 mx-auto w-fit'>
-            {characters.map((c) => (
-              <div
-                key={c.name}
-                className={`box rounded-[20px] border ${c.borderColor} w-fit bg-white p-7.5`}
-              >
-                <div className='flex gap-2 items-center'>
-                  <div className='text-brown-light text-xl my-2'>{c.name}</div>
-                  <Tag color={c.tagColor}>{c.tagLabel}</Tag>
-                </div>
 
-                <div className='font-normal text-brown-light leading-[1.4] font-sans text-sm mb-5'>
-                  {c.description}
-                </div>
-                <div className='flex flex-col gap-1 font-sans'>
-                  {c.traits.map((trait) => (
-                    <div key={trait} className='flex gap-2 items-center'>
-                      <div
-                        className={`w-1.5 h-1.5 rounded-full ${c.dotColor}`}
-                      ></div>
-                      <div className='text-muted text-sm'>{trait}</div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
+        {/* 첫번째 카드 */}
+        <div className='box bg-primary-light w-full py-15 font-jalnan'>
+          <div className='mb-4 text-2xl text-center text-primary-focus'>
+            공동 여행 계획 서비스
           </div>
-          <button
-            onClick={openLoginModal}
-            className='cursor-pointer text-white text-xl hover:bg-[#bc9d79] px-14 py-4 rounded-full mt-10 block w-fit mx-auto bg-caramel transition duration-200'
-          >
-            나는 카피? 바라? 테스트 해보기
-          </button>
+          <div className='text-center text-4xl text-primary leading-[1.4]'>
+            친구들과 함께 여행 계획을
+            <br />
+            세워보세요!
+          </div>
+          {/* 나중에 화면 캡쳐해서 쓸거임 */}
+        </div>
+        {/* 두번째 카드 */}
+        <div className='box bg-white w-full py-15 font-jalnan'>
+          <div className='text-center text-4xl text-sub'>이런걸 도와드려요</div>
+
+          <div className='w-fit flex gap-7.5 mx-auto mt-15'>
+            {features.map(
+              ({ icon: Icon, bg, iconColor, title, description }) => (
+                <div
+                  key={title}
+                  className={`box w-80 h-72 rounded-[20px] px-10 py-7.5 ${bg} flex flex-col items-center gap-5`}
+                >
+                  <div className='w-14 h-14 bg-white rounded-full flex items-center justify-center'>
+                    <Icon className={`w-8 h-8 ${iconColor}`} />
+                  </div>
+                  <div className='text-[20px] text-center text-sub'>
+                    {title}
+                  </div>
+                  <div className='font-sans font-normal text-center text-sub'>
+                    {description}
+                  </div>
+                </div>
+              ),
+            )}
+          </div>
+        </div>
+
+        {/* 세번째 카드 */}
+        <div className='box bg-caramel-light w-full py-15 font-jalnan'>
+          <div className='mb-4 text-2xl text-center text-brown'>
+            공동 여행 계획 서비스
+          </div>
+          <div className='mb-4 text-center text-4xl text-caramel'>
+            카피 & 바라를 소개합니다
+          </div>
+          <div className='font-sans font-normal text-center text-brown'>
+            서로 다른 여행 스타일을 가진 두 친구, 당신은 누구와 더 닮았나요?
+          </div>
+          <div className='flex gap-4 justify-center mt-15'>
+            <Image
+              src='/images/capi2.webp'
+              alt='Capi Image'
+              width={222}
+              height={295}
+              loading='eager'
+            />
+            <Image
+              src='/images/bara2.webp'
+              alt='Bara Image'
+              width={222}
+              height={295}
+              loading='eager'
+            />
+          </div>
+          <div>
+            <div className='flex gap-15 mt-15 mx-auto w-fit'>
+              {characters.map((c) => (
+                <div
+                  key={c.name}
+                  className={`box rounded-[20px] border ${c.borderColor} w-fit bg-white p-7.5`}
+                >
+                  <div className='flex gap-2 items-center'>
+                    <div className='text-brown-light text-xl my-2'>
+                      {c.name}
+                    </div>
+                    <Tag color={c.tagColor}>{c.tagLabel}</Tag>
+                  </div>
+
+                  <div className='font-normal text-brown-light leading-[1.4] font-sans text-sm mb-5'>
+                    {c.description}
+                  </div>
+                  <div className='flex flex-col gap-1 font-sans'>
+                    {c.traits.map((trait) => (
+                      <div key={trait} className='flex gap-2 items-center'>
+                        <div
+                          className={`w-1.5 h-1.5 rounded-full ${c.dotColor}`}
+                        ></div>
+                        <div className='text-muted text-sm'>{trait}</div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <button
+              onClick={openLoginModal}
+              className='cursor-pointer text-white text-xl hover:bg-[#bc9d79] px-14 py-4 rounded-full mt-10 block w-fit mx-auto bg-caramel transition duration-200'
+            >
+              나는 카피? 바라? 테스트 해보기
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
