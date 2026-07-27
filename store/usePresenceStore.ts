c983acff5586982ee68e42c3d137deaf5214747f
Ourumo <ss8638@naver.com>
2026-07-27 18:49:45 +0900

    feat: usePresenceStore에 내 식별자(clientId/userId) 저장 기능 추가

--- store/usePresenceStore.ts
+++ store/usePresenceStore.ts
@@ -4,14 +4,21 @@ import { PlanPresenceMember } from "@/types/presence";
 interface PresenceState {
   // 현재 계획 채널에 접속해 있는 참여자 목록 (본인 포함)
   members: PlanPresenceMember[];
+  // 내 식별자. clientId는 해당 탭, userId는 본인 id 단위
+  myClientId: string | null;
+  myUserId: string | null;
 
   setMembers: (members: PlanPresenceMember[]) => void;
-  clearMembers: () => void;
+  setMe: (clientId: string, userId: string) => void;
+  clearPresence: () => void;
 }
 
 export const usePresenceStore = create<PresenceState>((set) => ({
   members: [],
+  myClientId: null,
+  myUserId: null,
 
   setMembers: (members) => set({ members }),
-  clearMembers: () => set({ members: [] }),
+  setMe: (clientId, userId) => set({ myClientId: clientId, myUserId: userId }),
+  clearPresence: () => set({ members: [], myClientId: null, myUserId: null }),
 }));
